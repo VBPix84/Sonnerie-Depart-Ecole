c9170f40c033cb30ba8666811ea879582a037246
Vincent Besnard <vincent@vb-connect.fr>
2022-10-01 17:47:58 +0200

    Restructuration de la gestion 'Mode Admin'

--- index.php
+++ index.php
@@ -42,22 +42,18 @@ Initialisation des variables :
 
   - $nomDuFichier => Nom du fichier calendrier
   - $parsedJson => Le fichier json décodé par la fonction est intégré dans la variable
-  - $nbDates => Nombre de jour à afficher en "mode normal"
+  - $nbDatesAffichage => Nombre de jour à afficher en "mode normal"
 */
 $nomDuFichier = "calDepartEcoleV2.json";
 $parsedJson = parseJson($nomDuFichier);
-$nbDates = 14;
+$nbDatesAffichage = 14;
 
 /*
 Modes d'affichages : 
 
-  - Normal : $_GET['admin'] n'existent pas // Affiche la semaine qui arrive (= $nbDates)
+  - Normal : $_GET['admin'] n'existent pas // Affiche la semaine qui arrive (= $nbDatesAffichage)
   - Admin : $_GET['admin'] == true // Affiche toutes les dates dispo dans le json ("-1 pour enlever la date du jour")
 */
-if(isset($_GET['admin']) == true) {
-  $nbDates = count($parsedJson) - 1;
-    echo '<div class="text-center mt-3"><span class="badge bg-danger">Mode Administrateur</span></div>';
-}
 
 /*
 Traitement du formulaire via $_POST
@@ -69,114 +65,163 @@ pour le JSON (le fichier est COMPLETEMENT réécrit puis ré-enregistré)
 */
 ?>
 
-<div class="container col-10 col-sm-5 my-3 content-align-center">
+<div class="container col-10 col-sm-5 mt-3 content-align-center" style="margin-bottom : 75px;"> <!-- div qui contient le contenu -->
+
+<?php
+
+if(isset($_GET['admin']) == true) {
+  $nbDatesAffichage = 60; // Nombre de date dans le fichier json = count($parsedJson);
+  $nbDatesTotal = count($parsedJson);
+  $nbDatesDepassees = 5;
+  $btnDisabled = NULL;
+
+  if($nbDatesDepassees >= 20) {
+    $styleDateDepassee = "bg-danger";
+    $stylebtnPurge = "btn-danger";
+  } elseif ($nbDatesDepassees > 0) {
+    $styleDateDepassee = "bg-warning";
+    $stylebtnPurge = "btn-warning";
+  } elseif ($nbDatesDepassees == 0) {
+    $styleDateDepassee = "bg-success";
+    $stylebtnPurge = "btn-outline-success";
+    $btnDisabled = 'disabled';
+  }
+
+?>
+  <div class="card my-3">
+    <div class="card-header text-center">
+      <b>Administration</b>
+    </div>
+    <div class="card-body">
+        <ul class="list-group col-12">
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            Nb total d'entrées
+            <span class="badge bg-primary"><?php echo $nbDatesTotal; ?></span>
+          </li>
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            Nb d'entrées affichées
+            <span class="badge bg-primary"><?php echo $nbDatesAffichage; ?></span>
+          </li>
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            Nb de dates dépassées
+            <span class="badge <?php echo $styleDateDepassee; ?>"><?php echo $nbDatesDepassees; ?></span>
+          </li>
+        </ul>
+        <p class="list-group-item d-flex justify-content-center align-items-center mt-3">
+          <button class="btn btn-sm <?php echo $stylebtnPurge; ?>" type="submit" <?php echo $btnDisabled; ?>>Purger les dates dépassées</button>
+        </p>
+
+    </div>
+  </div>
+<?php
+}
+?>
+
 <div class="card">
   <div class="card-header text-center">
     <b>Départ Ecole</b>
   </div>
   <div class="card-body">
 
-<?php
+          <?php
 
-if(isset($_GET['admin']) == true) {
-  $nbDates = count($parsedJson) - 1;
-    echo '<div class="text-center mb-3"><span class="badge bg-warning text-dark">'.$nbDates.' entrées affichées</span></div>';
-}
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+          if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-  $enregistrement = array();
+            $enregistrement = array();
 
-  /*
-  Boucle for
+            /*
+            Boucle for
 
-  Boucle sur chaque entrée du formulaire:
-    1. $_POST pour la date en cours existe : Elle est donc à true, on push $enregistrement[$dateJourYMD] = true
-    2. $_POST pour la date en cours n'existe pas, on push $enregistrement[$dateJourYMD] = false
-     
-  On merge les 2 tableaux : L'ancien + Le nouveau pour ne mettre à jours QUE les entrées du formulaire, on ne
-  touche pas aux autres
+            Boucle sur chaque entrée du formulaire:
+              1. $_POST pour la date en cours existe : Elle est donc à true, on push $enregistrement[$dateJourYMD] = true
+              2. $_POST pour la date en cours n'existe pas, on push $enregistrement[$dateJourYMD] = false
+              
+            On merge les 2 tableaux : L'ancien + Le nouveau pour ne mettre à jours QUE les entrées du formulaire, on ne
+            touche pas aux autres
 
-  Puis on intègre le tableau merge dans le fichier avec file_put_contents();
+            Puis on intègre le tableau merge dans le fichier avec file_put_contents();
 
-  */
+            */
 
-  for($i = 0; $i <= $nbDates; $i++) { 
-    
-    $dateJourYMD = formaterDate($i, 'Y-m-d');
-    $enregistrement[$dateJourYMD] = (array_key_exists($dateJourYMD, $_POST)) ? true : false;
-  }
-  $aEnregistrer = array_merge($parsedJson, $enregistrement);
-  file_put_contents($nomDuFichier, json_encode($aEnregistrer));
+            for($i = 0; $i <= $nbDatesAffichage; $i++) { 
+              
+              $dateJourYMD = formaterDate($i, 'Y-m-d');
+              $enregistrement[$dateJourYMD] = (array_key_exists($dateJourYMD, $_POST)) ? true : false;
+            }
+            $aEnregistrer = array_merge($parsedJson, $enregistrement);
+            file_put_contents($nomDuFichier, json_encode($aEnregistrer));
 
-?>
+          ?>
 
         <!-- on affiche l'alerte : Données enregistrées -->
         <div class="alert alert-success d-flex align-items-center" role="alert">
           <div>
-          <i class="bi bi-check-circle-fill"></i>&nbsp;Données enregistrées !
+            <i class="bi bi-check-circle-fill"></i>&nbsp;Données enregistrées !
           </div>
         </div>
-<?php
+        
+          
+        <?php
 
-}
+          }
 
-/*
-Affichage du formulaire :
+          /*
+          Affichage du formulaire :
 
-  Pour chaque entrée selon le mode d'affichage (Toutes les entrées ou 14 jours):
-    - $dateJourYMD => Une case à cocher nommée avec la date format AAAAMMJJ
-    - $dateFrançaise => variable stockant la date en format FR à afficher
-    - $declenchement => Stock 'checked' si la valeur est true pour cocher la case
-    - $i => On commence à 1 pour commencer à demain...
+            Pour chaque entrée selon le mode d'affichage (Toutes les entrées ou 14 jours):
+              - $dateJourYMD => Une case à cocher nommée avec la date format AAAAMMJJ
+              - $dateFrançaise => variable stockant la date en format FR à afficher
+              - $declenchement => Stock 'checked' si la valeur est true pour cocher la case
+              - $i => On commence à 1 pour commencer à demain...
 
-  Puis le bouton d'enregistrement (envoi du formulaire en mode $_POST[])
+            Puis le bouton d'enregistrement (envoi du formulaire en mode $_POST[])
 
-  On rappelle la fonction $parsenJson pour actualiser l'affichage même après l'envoi du formulaire
-*/
+            On rappelle la fonction $parsenJson pour actualiser l'affichage même après l'envoi du formulaire
+          */
 
-echo '<form method="post" action="">';
+          echo '<form method="post" action="#">';
 
-$parsedJson = parseJson($nomDuFichier);
+          $parsedJson = parseJson($nomDuFichier);
 
-for($i = 1; $i < $nbDates +1; $i++) {
+          for($i = 1; $i < $nbDatesAffichage +1; $i++) {
 
-  // Création de la date format AAMMJJ
-  $dateJourYMD = formaterDate($i, 'Y-m-d');
+            // Création de la date format AAMMJJ
+            $dateJourYMD = formaterDate($i, 'Y-m-d');
 
-  // Pour passer en français la date affichée
-  $listeSemaine = array("Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi");
-  $listeMois = array(1=>"janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "decembre");
-  $dateFrancaise = $listeSemaine[formaterDate($i, 'w')].' '.formaterDate($i, 'j').' '.$listeMois[formaterDate($i, 'n')];
-  
-  $declenchement = ($parsedJson[$dateJourYMD] == true) ? 'checked':'';
+            // Pour passer en français la date affichée
+            $listeSemaine = array("Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi");
+            $listeMois = array(1=>"janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "decembre");
+            $dateFrancaise = $listeSemaine[formaterDate($i, 'w')].' '.formaterDate($i, 'j').' '.$listeMois[formaterDate($i, 'n')];
+            
+            $declenchement = ($parsedJson[$dateJourYMD] == true) ? 'checked':'';
 
-  // Affiche la checkbox de la date
-  echo '<div class="form-check form-switch">';
-  echo '<input class="form-check-input" type="checkbox" role="switch" name="'.$dateJourYMD.'" id="'.$dateJourYMD.'" '.$declenchement.'>';
-  echo '<label class="form-check-label" for="'.$dateJourYMD.'">'.$dateFrancaise.'</label>';
-  echo '</div>';
-}
-
-/*
-Mode d'affichage : 
-
-  - Debug : $_GET['debug'] == true // Affiche le array du json
-*/
-if(isset($_GET['debug']) == true) {
-  echo '<pre>';
-  print_r($parsedJson);
-  echo '</pre>';
-}
-?>
-
-        </div>
-        <nav class="navbar fixed-bottom navbar-dark bg-dark">
-          <div class="container-fluid  justify-content-center ">
-          <button class="btn btn-outline-light" type="submit" id="btnEnregistrer">Enregistrer</button>
-          </div>
-        </nav>
-      </form>
+            // Affiche la checkbox de la date
+            echo '<div class="form-check form-switch">';
+            echo '  <input class="form-check-input" type="checkbox" role="switch" name="'.$dateJourYMD.'" id="'.$dateJourYMD.'" '.$declenchement.'>';
+            echo '  <label class="form-check-label" for="'.$dateJourYMD.'">'.$dateFrancaise.'</label>';
+            echo '</div>';
+          }
+          ?>
+            <nav class="navbar fixed-bottom navbar-dark bg-dark">
+    <div class="container-fluid  justify-content-center ">
+      <button class="btn btn-outline-light" type="submit" id="btnEnregistrer">Enregistrer</button>
     </div>
+  </nav>
+        </form>
+      </div>
+    </div>
+    <?php 
+        if(!isset($_GET['admin'])) { // On affiche le bouton uniquement si nous ne sommes pas déjà en mode administrateur
+      ?>
+        <div class="text-center py-3">
+          <form method="get" action="">
+            <input name="admin" value="true" type="hidden">
+            <button class="btn btn-sm btn-outline-danger" id="admin" type="submit">Mode Administrateur</button>
+          </form>
+        </div>
+      <?php
+        }
+      ?>
   </body>
 </html>
